6a49aa792ed2873576895e528d5be17219874e3b
Антон <Ryrik17@gmail.com>
2026-01-13 02:48:46 +0300

    Initial commit: Setup Spring Boot, Postgres, Docker and Liquibase

---
+++ src/main/java/com/example/demo/CinemaPlusApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaPlusApplication.class, args);
+	}
+
+}
