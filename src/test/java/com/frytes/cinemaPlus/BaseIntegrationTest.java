4c6eb8bc116793f6d909a35c325e9a818e1e2f0b
Антон <Ryrik17@gmail.com>
2026-02-01 17:43:28 +0300

    Set ActiveProfiles for tests

--- src/test/java/com/frytes/cinemaPlus/BaseIntegrationTest.java
+++ src/test/java/com/frytes/cinemaPlus/BaseIntegrationTest.java
@@ -2,10 +2,12 @@ package com.frytes.cinemaPlus;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @Import(TestcontainersConfiguration.class)
 @Transactional
+@ActiveProfiles("test")
 public abstract class BaseIntegrationTest {
 }
