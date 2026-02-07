f22c4bda1b02dd9bf75fc3532aa7e9c27534729b
Антон <Ryrik17@gmail.com>
2026-02-07 15:05:07 +0300

    Fix: n+1

--- src/main/java/com/frytes/cinemaPlus/content/repository/SessionRepository.java
+++ src/main/java/com/frytes/cinemaPlus/content/repository/SessionRepository.java
@@ -1,6 +1,7 @@
 package com.frytes.cinemaPlus.content.repository;
 
 import com.frytes.cinemaPlus.content.entity.Session;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
@@ -8,7 +9,10 @@ import java.util.List;
 
 public interface SessionRepository extends JpaRepository<Session, Long> {
     boolean existsByHallIdAndStartTimeBeforeAndEndTimeAfter(Long id, LocalDateTime end, LocalDateTime start);
+
+    @EntityGraph(attributePaths = {"movie", "hall"})
     List<Session> findByStartTimeBetween(LocalDateTime start, LocalDateTime end);
+
     List<Session> findByMovieIdAndStartTimeAfter(Long movieId, LocalDateTime now);
     boolean existsByHallIdAndStartTimeBeforeAndEndTimeAfterAndIdNot(
             Long hallId, LocalDateTime endTime, LocalDateTime startTime, Long id
